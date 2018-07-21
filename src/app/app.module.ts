157f38f87a518ccb19e327ac828eeede64f59f9a
Shlomi <shlomik@gmail.com>
2018-07-21 13:10:36 +0300

    generating all components templated code using the cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingComponent,
+    RecipeDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
