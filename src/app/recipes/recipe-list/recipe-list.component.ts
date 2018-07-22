27c7dc1737ff5875a15fdfe9dddbe1d1e93cc2c6
Shlomi <shlomik@gmail.com>
2018-07-22 12:58:08 +0300

    adding package random-words to generate random recipe description

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {Recipe} from './recipe-item/recipe.object';
 import {SelectRecipeService} from '../select-recipe.service';
 
+declare var require: any;
+var randomWords = require('random-words');
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -9,9 +12,9 @@ import {SelectRecipeService} from '../select-recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('name', 'recipe description', 'http://energy106.ca/wp-content/uploads/2017/11/Junk-Food.jpg'),
-    new Recipe('name2', 'recipe description2', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/6/0/FN_snapchat_coachella_wingman%20.jpeg.rend.hgtvcom.616.462.suffix/1523633513292.jpeg'),
-    new Recipe('name3', 'recipe description4', 'https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg'),
+    new Recipe('name', randomWords({ exactly: 50, join: ' ' }), 'http://energy106.ca/wp-content/uploads/2017/11/Junk-Food.jpg'),
+    new Recipe('name2', randomWords({ exactly: 50, join: ' ' }), 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/6/0/FN_snapchat_coachella_wingman%20.jpeg.rend.hgtvcom.616.462.suffix/1523633513292.jpeg'),
+    new Recipe('name3', randomWords({ exactly: 50, join: ' ' }), 'https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg'),
   ];
 
   constructor(private selectRecipeService: SelectRecipeService) { }
